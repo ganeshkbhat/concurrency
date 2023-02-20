5988925a52290618ebda7aef039a7c63ef14b015
Ganesh K Bhat <ganeshsurfs@gmail.com>
2023-02-20 13:33:06 +0530

    [COMMIT] [concurrency.js] Adding TODO for concurrency.js. Adding images for Threads and Process architecture. Adding minor changes to TODO

---
+++ src/worker.cluster.threads.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ * Package: concurrency.js
+ * Author: Ganesh B
+ * Description: npm module to work with concurrency - worker threads and worker processes easily using simple functions and script files
+ * Install: npm i concurrency.js --save
+ * Github: https://github.com/ganeshkbhat/concurrency
+ * npmjs Link: https://www.npmjs.com/package/
+ * File: worker.cluster.threads.js
+ * File Description: 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+
+const path = require("path");
+const fs = require("fs");
+
