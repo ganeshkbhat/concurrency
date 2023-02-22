ce9153dabc7d1112c194dfec8bb021c6a666cc7b
Ganesh K Bhat <ganeshsurfs@gmail.com>
2023-02-22 17:36:46 +0530

    [TEMP] Minor changes to demos and test files

--- test/demos.threads.js
+++ test/demos.threads.js
@@ -9,14 +9,22 @@ let { _concurrencyThreads } = require("../index.js");
 // _concurrencyThreads(path.join("C:\\Users\\GB\\Documents\\projects\\requireurl\\concurrency\\src\\worker.threads.js"), { data: { url: "https://www.google.com", message: "Testing data" } });
 
 async function concurrency() {
-    let c = await _concurrencyThreads(__filename, {
-        data: {
-            url: "https://www.google.com",
-            message: "Testing data"
-        },
-        childData: "Testing child data"
-    }, true)
-    return c;
+    var responses;
+    async function testPromise() {
+        try {
+            responses = await _concurrencyThreads(__filename, {
+                data: {
+                    message: "Testing data",
+                    url: "https://www.google.com"
+                },
+                childData: "Testing child data"
+            });
+            return responses;
+        } catch (e) {
+            return e;
+        }
+    };
+    return await testPromise();
 }
 
-module.exports.concurrency = concurrency;
+module.exports.concurrency = concurrency();
--- test/test_demos_threads.js
+++ test/test_demos_threads.js
@@ -24,121 +24,17 @@ var spawn = require("child_process").spawn;
 const { _concurrencyThreads } = require("../index.js");
 
 
-// describe('test-.js::concurrency.js: [Test A] Test Suite for concurrency.js .threads in main repo directory', function () {
+describe('test-.js::concurrency.js: [Test A] Test Suite for concurrency.js .threads in main repo directory', function () {
 
-//     // it('[Test A] Test for threads ', function (done) {
-//     //     var responses;
-//     //     _concurrencyThreads(__filename, {
-//     //         data: {
-//     //             message: "Testing data",
-//     //             url: "https://www.google.com"
-//     //         },
-//     //         childData: "Testing child data"
-//     //     }).then((d) => {
-//     //         console.log(JSON.stringify(d));
-//     //         expect(200).to.equal(100);
-//     //         responses = d;
-//     //         expect(responses).to.equal({
-//     //             "message": [
-//     //                 { "pid": responses.messages[0].pid, "message": "\"Hello from child. - Thread: " + responses.messages[0].pid + "", "threadId": 1 },
-//     //                 {
-//     //                     "closeChild": true,
-//     //                     "pid": responses.messages[0].pid,
-//     //                     "childMessageData": [
-//     //                         { "pid": responses.messages[0].pid, "message": "Hello, world! - Server: 11660" },
-//     //                         { "closeChild": true }],
-//     //                     "result": [],
-//     //                     "threadId": 1
-//     //                 }
-//     //             ],
-//     //             "result": []
-//     //         });
-//     //         done();
-//     //     });
+  it('[Test A] Test for threads _concurrencyThreads', async function () {
+    let responses = await require("./demos.threads").concurrency;
+    
+    expect(Object.keys(responses).length).to.equal(2);
+    expect(responses.message.length).to.equal(1);
+    // expect(Object.keys(responses.message[0]).length).to.equal(2);
+    expect(Object.keys(responses.message[0]).length).to.equal(5);
+  });
 
-//     //     // done();
-//     // });
+});
 
 
-// });
-
-
-const assert = require('assert');
-const concurrency = require('concurrency.js');
-
-// // describe('Concurrency', function() {
-
-// describe('#_concurrencyThreads()', function () {
-//   // it('should return a promise that resolves with the message data and result when called with valid arguments', async function() {
-//   //   try {
-//   //     const result = await concurrency._concurrencyThreads(__filename, {
-//   //         data: {
-//   //             message: "Testing data",
-//   //             url: "https://www.google.com"
-//   //         },
-//   //         childData: "Testing child data"
-//   //     }, false, describe);
-//   //       assert.strictEqual(result.message.length, 2);
-//   //       assert.strictEqual(result.result.length, 1);
-//   //   } catch(e) {
-//   //     assert.equal(e, undefined);
-//   //   }
-//   // });
-
-//   it('should return a promise that resolves with the message data and result when called with valid arguments', async function () {
-//     var func = require("./demos.threads").concurrency;
-//     try {
-//       const result = await func();
-//       console.log(result);
-//       assert.strictEqual(result.message.length, 2);
-//       assert.strictEqual(result.result.length, 1);
-//       // done();
-//     } catch (e) {
-//       assert.equal(e, undefined);
-//       // done();
-//     }
-//   });
-
-//   //   it('should reject with an error when called with an invalid file path', async function() {
-//   //     await assert.rejects(async function() {
-//   //       await concurrency._concurrencyThreads('./invalid/path/to/worker.js', {
-//   //           data: {
-//   //               message: "Testing data",
-//   //               url: "https://www.google.com"
-//   //           },
-//   //           childData: "Testing child data"
-//   //       }, false);
-//   //     }, "Error: Cannot find module 'C:\\Users\\GB\\Documents\\projects\\requireurl\\concurrency\\invalid\\path\\to\\worker.js'");
-//   //   });
-//   // });
-
-//   //   describe('#_concurrencyProcesses()', function() {
-//   //     it('should return a promise that resolves with the message data and result when called with valid arguments', async function() {
-//   //       const result = await concurrency._concurrencyProcesses(__filename, {
-//   //         data: {
-//   //             message: "Testing data",
-//   //             url: "https://www.google.com"
-//   //         },
-//   //         childData: "Testing child data"
-//   //     }, false);
-//   //       assert.strictEqual(result.message.length, 2);
-//   //       assert.strictEqual(result.result.length, 1);
-//   //     });
-
-//   //     it('should reject with an error when called with an invalid file path', async function() {
-//   //       await assert.rejects(async function() {
-//   //         await concurrency._concurrencyProcesses('invalid/path/to/worker.js', {}, false);
-//   //       }, /Worker failed to spawn/);
-//   //     });
-//   //   });
-
-// });
-
-async function test() {
-  var func = require("./demos.threads").concurrency;
-  const result = await func();
-  console.log(result);
-  return result;
-}
-
-test();
