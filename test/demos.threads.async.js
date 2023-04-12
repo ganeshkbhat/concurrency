c7f1ec6f11c99ca09eb5dd664d740ba0a3df93bc
Ganesh K Bhat <ganeshsurfs@gmail.com>
2023-04-12 06:58:24 +0530

    [TEMP] Adding Minor changes to threadasync.js and tests for process. Test for threads failing due to system locking up

--- test/demos.threads.async.js
+++ test/demos.threads.async.js
@@ -1,44 +1,44 @@
-// /**
-//  * 
-//  * Package: concurrency.js
-//  * Author: Ganesh B
-//  * Description: npm module to work with concurrency - worker threads and worker processes easily using simple functions and script files
-//  * Install: npm i  --save
-//  * Github: https://github.com/ganeshkbhat/concurrency
-//  * npmjs Link: https://www.npmjs.com/package/concurrency.js
-//  * File: test.demos.cluster.js
-//  * File Description: 
-//  * 
-// */
+/**
+ * 
+ * Package: concurrency.js
+ * Author: Ganesh B
+ * Description: npm module to work with concurrency - worker threads and worker processes easily using simple functions and script files
+ * Install: npm i  --save
+ * Github: https://github.com/ganeshkbhat/concurrency
+ * npmjs Link: https://www.npmjs.com/package/concurrency.js
+ * File: test.demos.cluster.js
+ * File Description: 
+ * 
+*/
 
-// /* eslint no-console: 0 */
-
-
-// 'use strict';
-
-// const path = require("path");
-// let { _concurrencyThreadsAsync } = require("../index.js");
+/* eslint no-console: 0 */
 
 
-// async function concurrency() {
-//     var responses;
-//     async function testPromise() {
-//         try {
-//             responses = await _concurrencyThreadsAsync(__filename, {
-//                 data: {
-//                     message: "Testing data",
-//                     url: "https://www.google.com"
-//                 },
-//                 childData: "Testing child data"
-//             }, true);
-//             return responses;
-//         } catch (e) {
-//             console.log(e);
-//             return e;
-//         }
-//     };
-//     return await testPromise();
-// }
+'use strict';
 
-// module.exports.concurrency = concurrency();
+const path = require("path");
+let { _concurrencyThreadsAsync } = require("../index.js");
+
+
+async function concurrency() {
+    var responses;
+    async function testPromise() {
+        try {
+            responses = await _concurrencyThreadsAsync(__filename, {
+                data: {
+                    message: "Testing data",
+                    url: "https://www.google.com"
+                },
+                childData: "Testing child data"
+            }, true);
+            return responses;
+        } catch (e) {
+            console.log(e);
+            return e;
+        }
+    };
+    return await testPromise();
+}
+
+module.exports.concurrency = concurrency();
 
